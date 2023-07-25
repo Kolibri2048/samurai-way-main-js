a6e239d2b02d4050af613e281e0f910d83056e42
Kolibri2048 <blak_fog@mail.ru>
2023-07-25 23:48:31 +0200

    lessons video 13

---
+++ src/components/Profile.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src='https://www.photoshopbuzz.com/wp-content/uploads/change-color-part-of-image-psd4.jpg'/>
+            </div>
+            <div>
+                ava + descr
+            </div>
+            <div>
+                My posts
+                <div>
+                    New Post
+                </div>
+                <div>
+                    <div>
+                        post1
+                    </div>
+                    <div>
+                        post2
+                    </div>
+                </div>
+            </div>
+
+        </div>
+)
+}
+
+export default Profile
