bc260ff9a3594648f8cda1b69bf3596dabdcf689
Kolibri2048 <blak_fog@mail.ru>
2023-08-05 21:04:02 +0200

    Folder structure: creted new folder

---
+++ src/components/Profile/Profile.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import s from './Profile.module.css'
+import MyPosts from "./MyPosts/MyPosts";
+const Profile = () => {
+    return (
+        <div className={s.content}>
+            <div>
+                <img src='https://www.photoshopbuzz.com/wp-content/uploads/change-color-part-of-image-psd4.jpg'/>
+            </div>
+            <div>
+                ava + descr
+            </div>
+            <MyPosts />
+        </div>
+)
+}
+
+export default Profile
