859675fc864bb265876ac6827b1e589ceb495274
Kolibri2048 <blak_fog@mail.ru>
2023-12-05 04:33:17 +0100

    less 35 callback subsribe observer - protokol

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import './index.css';
-import {rerenderEntireTree} from "./render";
-import state from "./redux/state";
+import state, {RootStateType, subscribe} from "./redux/state";
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 
+
+let rerenderEntireTree = (state: RootStateType) => {
+    ReactDOM.render(
+        <App />,
+        document.getElementById('root')
+    );
+}
 
 rerenderEntireTree(state)
+
+subscribe(rerenderEntireTree)
+
 
 
--- src/redux/state.tsx
+++ src/redux/state.tsx
@@ -1,4 +1,6 @@
-import {rerenderEntireTree} from "../render";
+let rerenderEntireTree = (state: RootStateType) => {
+    console.log('state changing')
+}
 
 export type MessageType = {
     id: number;
@@ -38,35 +40,35 @@ export type updateNewPostTextType = (newText: string) => void
 let state: RootStateType = {
     profilePage: {
         posts: [
-            { id: 1, message: 'Hi, How are you', likesCount: 12 },
-            { id: 2, message: 'It\'s my first post', likesCount: 23 },
-            { id: 3, message: 'It\'s my first post', likesCount: 23 },
-            { id: 4, message: 'It\'s my first post', likesCount: 2 },
+            {id: 1, message: 'Hi, How are you', likesCount: 12},
+            {id: 2, message: 'It\'s my first post', likesCount: 23},
+            {id: 3, message: 'It\'s my first post', likesCount: 23},
+            {id: 4, message: 'It\'s my first post', likesCount: 2},
         ],
         newPostText: 'it-kamasutra.com'
     },
     dialogsPage: {
         dialogs: [
-            { id: 1, name: 'Dimych' },
-            { id: 2, name: 'Andrey' },
-            { id: 3, name: 'Viktor' },
-            { id: 4, name: 'Sveta' },
-            { id: 5, name: 'Sasha' },
-            { id: 6, name: 'Valera' }
+            {id: 1, name: 'Dimych'},
+            {id: 2, name: 'Andrey'},
+            {id: 3, name: 'Viktor'},
+            {id: 4, name: 'Sveta'},
+            {id: 5, name: 'Sasha'},
+            {id: 6, name: 'Valera'}
         ],
 
         messages: [
-            { id: 1, message: 'Hi' },
-            { id: 2, message: 'How is your it?' },
-            { id: 3, message: 'Yo' },
-            { id: 4, message: 'Yo' },
-            { id: 5, message: 'Yo' },
-            { id: 6, message: 'Yo' }
+            {id: 1, message: 'Hi'},
+            {id: 2, message: 'How is your it?'},
+            {id: 3, message: 'Yo'},
+            {id: 4, message: 'Yo'},
+            {id: 5, message: 'Yo'},
+            {id: 6, message: 'Yo'}
         ]
     },
 }
 
-export let addPost = () => {
+export const addPost = () => {
     let newPost: PostType = {
         id: 5,
         message: state.profilePage.newPostText,
@@ -77,9 +79,13 @@ export let addPost = () => {
     rerenderEntireTree(state)
 }
 
-export let updateNewPostText: updateNewPostTextType = (newText: string) => {
+export const updateNewPostText: updateNewPostTextType = (newText: string) => {
     state.profilePage.newPostText = newText
     rerenderEntireTree(state)
+}
+
+export const subscribe = (observer: (state: RootStateType) => void) => {
+    rerenderEntireTree = observer;
 }
 
 
